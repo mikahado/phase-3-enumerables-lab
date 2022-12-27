4afb6a12771e31e3e25cdf3af2866e4ea691d490
Michael <mikahado@gmail.com>
2022-12-27 10:08:24 -0700

    lab completed, code along

--- enumerables.rb
+++ enumerables.rb
@@ -12,9 +12,10 @@ end
 # given an array of spicy foods, **return an array of strings**
 # with the names of each spicy food
 def get_names(spicy_foods)
-  # your code here
+  spicy_foods.map do |food| food[:name]
 end
 
+binding.pry
 # given an array of spicy foods, **return an array of hashes** 
 # where the heat level of the food is greater than 5
 def spiciest_foods(spicy_foods)
